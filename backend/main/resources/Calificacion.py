4260b5cbe197b707975f8dfc0c7a55e30e7695e2
SantiagoZ359 <sn.zapata@alumno.um.edu.ar>
2022-04-14 16:47:52 -0300

    cambios recurso calificacion

--- backend/main/resources/Calificacion.py
+++ backend/main/resources/Calificacion.py
@@ -1,6 +1,9 @@
+import re
 import resource
 from flask_restful import Resource
-from flask import request
+from flask import request, jsonify
+from .. import db
+from main.models import CalificacionModel
 
 #Calificaciones
 
@@ -11,22 +14,21 @@ CALIFICACIONES = {
 
 class Calificacion(Resource):
     def get(self,id):
-        if int(id) in CALIFICACIONES:
-            return CALIFICACIONES[int(id)]
-        return '',404
+        calificacion = db.session.query(CalificacionModel).get_or_404(id)
+        return calificacion.to_jason()
     def delete(self,id):
-        if int(id) in CALIFICACIONES:
-            del CALIFICACIONES[int(id)]
-            return '',204
-        return '',404
+        calificacion = db.session.query(CalificacionModel).get_or_404(id)
+        db.session.delete(calificacion)
+        db.session.commit()
+        return '',204
 
 class Calificaciones(Resource):
     def get (self):
-        return CALIFICACIONES
+        calificaciones = db.session.query(CalificacionModel).all()
+        return jsonify ([Calificacion.to_json_short() for calificacion in calificaciones])
 
     def post(self):
-        Calificacion = request.get_json()
-        id = int(max(CALIFICACIONES.keys())) + 1
-        CALIFICACIONES[id] = Calificacion
-        return CALIFICACIONES[id], 201
-
+        calificacion = CalificacionModel.from_json(request.get_json())
+        db.session.add(calificacion)
+        db.session.commit()
+        return calificacion.to_json(), 201
