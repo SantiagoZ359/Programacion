3d98e53439f1c0f427840ea5a2427cea4831908a
SantiagoZ359 <sn.zapata@alumno.um.edu.ar>
2022-04-15 18:46:30 -0300

    tablas creadas, problemas al put poema

--- backend/main/models/Poema.py
+++ backend/main/models/Poema.py
@@ -1,21 +1,22 @@
 from datetime import datetime
+from sqlalchemy import PrimaryKeyConstraint, true
 from .. import db
 
 class Poema(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.String(50), nullable=False)
-    usuario_id = db.Column(db.Integer, nullable=False)
+    usuario_id = db.Column(db.Integer, primary_key=True)
     cuerpo =db.Column(db.String(500), nullable=False)
-    fecha =db.Column(db.Integer, nullable=False)
+    fecha =db.Column(db.Integer, primary_key=True)
     def __repr__(self):
         return '<Poema: %r %r %r %r >' % (self.titulo, self.usuario_id, self.cuerpo, self.fecha)
     def to_json(self):
         poema_json = {
             'id': self.id,
             'titulo': str(self.titulo),
-            'autor_id': int(self.usuario_id),
+            'usuario_id': self.usuario_id,
             'cuerpo': str(self.cuerpo),
-            'fecha': int(self.fecha),
+            'fecha': self.fecha,
         }
         return poema_json
 
@@ -23,22 +24,22 @@ class Poema(db.Model):
         poema_json = {
             'id': self.id,
             'titulo': str(self.titulo),
-            'autor_id': int(self.usuario_id),
+            'usuario_id': self.usuario_id,
             'cuerpo':str(self.cuerpo),
-            'fecha':int(self.fecha)
+            'fecha':self.fecha
         }
         return poema_json
     @staticmethod
     #Convertir JSON a objeto
     def from_json(poema_json):
         id = poema_json.get('id')
-        autor = poema_json.get('autor_id')
+        usuario_id = poema_json.get('usuario_id')
         titulo = poema_json.get('titulo')
         cuerpo = poema_json.get('cuerpo')
         fecha = poema_json.get('fecha')
         return Poema(id=id,
                     titulo=titulo,
-                    autor=autor,
+                    usuario_id=usuario_id,
                     cuerpo=cuerpo,
                     fecha=fecha,
                     )
