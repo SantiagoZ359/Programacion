76058c5e6a922eef3d048b256eb3ab13c4e66df7
SantiagoZ359 <sn.zapata@alumno.um.edu.ar>
2022-05-20 15:50:30 -0300

    empezando email

---
+++ backend/main/mail/functions.py
@@ -0,0 +1,21 @@
+from tkinter.tix import Tree
+from .. import mailsender
+from flask import current_app, render_template
+from flask_mail import Message
+from smtplib import SMTPException
+
+#Se crea la funcion para el envio de emails
+def sendMail(to, subject, template, **kwargs):
+    #configuro el email
+    msg = Message( subject, sender = current_app.config['FLASKY_MAIL_SENDER'], recipients=to)
+    try:
+        #creamos el cuerpo del mensaje.
+        msg.body = render_template(template + '.txt', **kwargs)
+        msg.html = render_template(template + '.html', **kwargs)
+        #enviamos el email
+        result = mailsender.send(msg)
+    except SMTPException as e:
+        print(str(e))
+        return "Envio de mail fallado"
+    return True
+    
