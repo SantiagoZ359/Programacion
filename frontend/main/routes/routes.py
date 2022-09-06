0b7a714fdaede762572cff20523fd62f12e4ca7c
SantiagoZ359 <sn.zapata@alumno.um.edu.ar>
2022-09-06 16:30:04 -0300

    tp7 terminado

--- frontend/main/routes/routes.py
+++ frontend/main/routes/routes.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template
 
 app = Blueprint('app', __name__, url_prefix= '/')
 
-@app.route('/')
+@app.route('/poeta')
 def index():
     return render_template ('pag_princ_poeta.html')
 
@@ -19,6 +19,26 @@ def editar_perfil():
 def lista_poemas_usuario():
     return render_template ('lista_poem_user.html')
 
-@app.route('/lista_poem_poeta')
+@app.route('/lista_poema_poeta')
 def lista_poem_poeta():
     return render_template ('lista_poema_poeta.html')
+
+@app.route('/user')
+def pag_princ_user():
+    return render_template ('pag_princ_user.html')
+
+@app.route('/perfil')
+def perfil():
+    return render_template ('perfil.html')
+
+@app.route('/subir_poema')
+def subir_poema():
+    return render_template ('subir_poema.html')
+
+@app.route('/ver_poema_poeta')
+def ver_poema_poeta():
+    return render_template ('ver_poema_poeta.html')
+
+@app.route('/ver_poema_user')
+def ver_poema_user():
+    return render_template ('ver_poema_user.html')
